6ab523409e9ca1da57fb06cd3ce15fae0297b0a4
David Perez Cruz <dperezc01@gmail.com>
2023-08-03 11:12:51 +0200

    created scaffold for users

--- Job Application Tracker Webapp/Data/Job_Application_Tracker_WebappContext.cs
+++ Job Application Tracker Webapp/Data/Job_Application_Tracker_WebappContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Job_Aplication_Tracker.Models;
+using Job_Application_Tracker_Webapp.Models;
 
 namespace Job_Application_Tracker_Webapp.Data
 {
@@ -15,5 +16,7 @@ namespace Job_Application_Tracker_Webapp.Data
         }
 
         public DbSet<Job_Aplication_Tracker.Models.Application> Application { get; set; } = default!;
+
+        public DbSet<Job_Application_Tracker_Webapp.Models.User>? User { get; set; }
     }
 }
